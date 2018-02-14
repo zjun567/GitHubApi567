ba344fbd59b972e83db7cb294d92af650d26abda
zjun567 <35976513+zjun567@users.noreply.github.com>
2018-02-13 22:13:03 -0500

    Update Test-api.py

--- Test-api.py
+++ Test-api.py
@@ -7,7 +7,7 @@ class TestApi(unittest.TestCase):
 
     def testApi(self):
         self.assertEqual(result('zjun567')[1], ['GitHubApi567', 'Triangle567'])
-        self.assertEqual(result('zjun567')[2], [1,6])
+        self.assertEqual(result('zjun567')[2], [4,6])
 
 if __name__ == '__main__':
     print('Running unit tests')
